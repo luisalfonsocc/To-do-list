b6483d5e7fd0efebe2963f4f066c29ec76e6b7e2
unknown <luisalfonsocc@gmail.com>
2023-11-28 15:34:02 -0300

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [item, setItem] = useState('');
+  const [list, setList] = useState([]);
+
+  const addElement = () => {
+    if (item.trim() !== '') {
+      setList([...list, item]);
+      setItem('');
+    }
+  };
+
+  const deleteItem = (index) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container paper">
+      <h1>TO DO LIST</h1>
+      <div className="paper">
+        <input
+          type="text"
+          value={item}
+          onChange={(event) => setItem(event.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              addElement();
+            }
+          }}
+          placeholder="Type Item"
+        />
+        <ul>
+          {list.map((element, index) => (
+            <li key={index}>
+              {element}
+              <button onClick={() => deleteItem(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+        <p>{list.length} item left</p>
+      </div>
     </div>
   );
 }
